ff8073cacb65d6d0e0b645387421c899bcd4b962
Muhammad Qasim <qasim.nouman850@gmail.com>
2024-05-05 23:54:06 +0500

    first commit:

---
+++ src/components/background-layout/background-layout.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { FC, useEffect, useState } from "react";
+import ClearWeather from "../../assets/images/Clear.jpg";
+import Fog from "../../assets/images/fog.jpg";
+import Cloudy from "../../assets/images/Cloudy.jpg";
+import Rainy from "../../assets/images/Rainy.jpg";
+import Snow from "../../assets/images/snow.jpg";
+import Stormy from "../../assets/images/Stormy.jpg";
+import Haze from "../../assets/images/haze.jpg";
+interface BackgroundLayoutProps {
+  iconString: string;
+}
+const BackgroundLayout: FC<BackgroundLayoutProps> = ({ iconString }) => {
+  const [image, setImage] = useState(ClearWeather);
+
+  useEffect(() => {
+    if (iconString) {
+      const imageString = iconString;
+      if (imageString?.toLowerCase().includes("clear")) {
+        setImage(ClearWeather);
+      } else if (imageString?.toLowerCase().includes("cloud")) {
+        setImage(Cloudy);
+      } else if (
+        imageString?.toLowerCase().includes("rain") ||
+        imageString?.toLowerCase().includes("shower")
+      ) {
+        setImage(Rainy);
+      } else if (imageString?.toLowerCase().includes("snow")) {
+        setImage(Snow);
+      } else if (imageString?.toLowerCase().includes("fog")) {
+        setImage(Fog);
+      } else if (
+        imageString?.toLowerCase().includes("thunder") ||
+        imageString?.toLowerCase().includes("storm")
+      ) {
+        setImage(Stormy);
+      } else if (imageString?.toLowerCase().includes("haze")) {
+        setImage(Haze);
+      }
+    }
+  }, [iconString]);
+
+  return (
+    <img
+      src={image}
+      alt="weather_image"
+      className="blur-sm	h-full w-full fixed left-0 top-0 -z-[10]"
+    />
+  );
+};
+
+export default BackgroundLayout;
